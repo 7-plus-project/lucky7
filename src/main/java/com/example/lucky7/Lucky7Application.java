8725cd141778686e8a67630bffc682d0782695bf
HoKirby <lakevely27@gmail.com>
2025-03-24 16:28:42 +0900

    init: initialize repository lucky7

---
+++ src/main/java/com/example/lucky7/Lucky7Application.java
@@ -0,0 +1,13 @@
+package com.example.lucky7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lucky7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lucky7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lucky7/Lucky7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lucky7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lucky7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
