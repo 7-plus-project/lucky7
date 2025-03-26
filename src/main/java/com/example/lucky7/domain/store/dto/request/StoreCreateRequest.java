02a53392cf8599ddca60189644dc3400385361dc
minjonyyy <minjonyyy@naver.com>
2025-03-26 18:00:29 +0900

    Impl: Mysql 위치기반 request 필드 추가 부분 제거(추후 KAKAO MAP API 사용 예정)

--- src/main/java/com/example/lucky7/domain/store/dto/request/StoreCreateRequest.java
+++ src/main/java/com/example/lucky7/domain/store/dto/request/StoreCreateRequest.java
@@ -16,8 +16,8 @@ public class StoreCreateRequest {
 
     private StoreCategory category;
 
-
-    private Double latitude;
-    private Double longitude;
+    // Mysql 위치기반 request 필드 추가 부분 제거(추후 KAKAO MAP API 사용 예정)
+//    private Double latitude;
+//    private Double longitude;
 
 }
