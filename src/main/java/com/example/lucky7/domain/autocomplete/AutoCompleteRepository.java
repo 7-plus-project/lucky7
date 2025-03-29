b372ebbcd4746e51b02d98c58bf766f769add6c9
Hokirby <lakevely27@gmail.com>
2025-03-29 17:22:14 +0900

    fix: 관련 없는 도메인 파일 삭제

--- src/main/java/com/example/lucky7/domain/autocomplete/AutoCompleteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.lucky7.domain.autocomplete;
-
-
-import java.util.List;
-
-public interface AutoCompleteRepository {
-    List<String> getAutoComplete(String prefix, int limit);
-}
--- src/main/java/com/example/lucky7/domain/autocomplete/dto/response/AutoCompleteResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.lucky7.domain.autocomplete.dto.response;
-
-import java.util.HashSet;
-
-public record AutoCompleteResponse(HashSet<String> results) {
-
-}
